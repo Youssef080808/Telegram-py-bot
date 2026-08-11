633b028407b65844ba707c95a9da48539e49a888
Youssef080808 <youssef.yasser.mansour@gmail.com>
2026-08-11 20:46:56 +0300

    Add /help, /random, and /feed with optional count

--- bot.py
+++ bot.py
@@ -15,12 +15,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
 import xml.etree.ElementTree as cElementTree
+import random
 
-BOT_TOKEN = "PASTE_TOKEN_HERE" 
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE" 
 
 # Parses first 10 items from http://planetpython.org/rss20.xml and returns a list of 
 # dictionaries with the title, link, and description of each item.
-def parse_planetpy_rss():
+def parse_planetpy_rss(number : int):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     response = requests.get("https://planetpython.org/rss20.xml", headers=headers) # GET request to download the 
     # RSS feed from the specified URL
@@ -37,24 +38,56 @@ def parse_planetpy_rss():
                     item["link"] = item_node.text
                     #EX:{'title': 'Some Blog Post Title', 'link': 'https://example.com/post'}
             items.append(item)
-    return items[:10]# return the first 10 items from the list of dictionaries
+    return items[:number]# return the first 10 items from the list of dictionaries
 
-# runs when someone send /start command to the bot
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hi! Send /feed to get the latest Python blog posts.")
-    #update contains info about incoming message, and bot replies back
+# runs when someone send /start command to the bot, update is info about incoming message and
+# context is extra tools and info, "starts the bot and sends a welcome message to the user"
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hi! Send /help to see all available commands.")
+    # update contains info about incoming message, and bot replies back
 
-# runs when someone send /feed command to the bot
-async def feed(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    posts = parse_planetpy_rss() # list of 10 dictionaries
+# runs when someone send /feed command to the bot, "gets 10 latest blog posts from planetpython.org"
+async def feed_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(context.args) > 0:
+        if context.args[0].isdigit():
+            number = int(context.args[0])
+            if number <= 0:
+                await update.message.reply_text("Please provide a valid positive number between 0-10")
+                return
+            elif number > 10:
+                await update.message.reply_text("Please provide a valid number between 0-10")
+                return
+        else:
+            await update.message.reply_text("Please provide a valid number after /feed command")
+            return
+    else:
+        number = 10
+    posts = parse_planetpy_rss(number) # list of 10 dictionaries
     message = "\n\n".join([f"{p['title']}\n{p['link']}" for p in posts]) # COMPACT LOOP to 
-    #create a string with the titles and links of the latest blog posts
+    # create a string with the titles and links of the latest blog posts
     await update.message.reply_text(message) # sends combined message back to the user 
     # with the titles and links of the latest blog posts
+# runs when someone send /help command to the bot, "shows current available commands"
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("These are the commands you can use:\n"
+    "/start: starts the bot\n"
+    "/feed x: gets the x latest blog posts from Planet Python or 10 if no number provided\n"
+    "/help: shows available commands\n"
+    "/random: gets a random blog post from Planet Python"
+    )
+async def random_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    latest_blogs = parse_planetpy_rss(10) # list of 10 dictionaries
+    random_blog = random.choice(latest_blogs) # selects a random blog post from the list
+    message = f"{random_blog['title']}\n{random_blog['link']}"
+    await update.message.reply_text(message) # sends the random blog post back to the user
+
+
 
 if __name__ == "__main__": # if this script is being run directly, then execute the code
     app = ApplicationBuilder().token(BOT_TOKEN).build() #creates a bot application
-    app.add_handler(CommandHandler("start", start)) # calls start() when user sends /start command
-    app.add_handler(CommandHandler("feed", feed)) # calls feed() when user sends /feed command
+    app.add_handler(CommandHandler("start", start_command)) # calls start_command() when user sends /start command
+    app.add_handler(CommandHandler("feed", feed_command)) # calls feed_command() when user sends /feed command
+    app.add_handler(CommandHandler("help", help_command)) # calls help_command() when user sends /help command
+    app.add_handler(CommandHandler("random", random_command)) # calls random_command() when user sends /random command
     print("Bot is running...")
     app.run_polling() # starts the bot and keeps it running
